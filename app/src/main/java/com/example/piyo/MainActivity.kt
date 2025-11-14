9a4489fafb0b38a4e16a375e82f82d7de9a39b36
ghufronbagaskara <ghufronbagas@student.ub.ac.id>
2025-11-14 13:09:57 +0700

    feat: add MainActivity for app entry point with navigation and theme setup

---
+++ app/src/main/java/com/example/piyo/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.piyo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import com.example.piyo.presentation.navigation.MainRoute
+import com.example.piyo.presentation.navigation.SplashRoute
+import com.example.piyo.presentation.navigation.TutorialNavigation
+import com.example.piyo.ui.theme.PiyoTheme
+import com.example.piyo.util.FirebaseUtils
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PiyoTheme {
+                val startDestination = if (FirebaseUtils.isUserLoggedIn()) MainRoute else SplashRoute
+//                TransparentSystemBars()
+                TutorialNavigation(startDestination = startDestination)
+            }
+        }
+    }
+}
+
+@Composable
+private fun TransparentSystemBars() {
+    val systemUiController = rememberSystemUiController()
+    SideEffect {
+        systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = true)
+    }
+}
