be679ae103bf1f4c59f440768911b55d3482ee2e
ghufronbagaskara <ghufronbagas@student.ub.ac.id>
2025-11-23 00:26:18 +0700

    feat: add QuizDto for data transfer

---
+++ app/src/main/java/com/example/piyo/data/dto/quiz/QuizDto.kt
@@ -0,0 +1,16 @@
+package com.example.piyo.data.dto.quiz
+
+import com.google.firebase.Timestamp
+
+data class QuizDto(
+    val id: String? = null,
+    val title: String? = null,
+    val thumbnail: String? = null,
+    val description: String? = null,
+    val totalQuestions: Int? = null,
+    val duration: Int? = null,
+    val difficulty: String? = null,
+    val createdAt: Timestamp? = null,
+    val tags: List<String>? = null
+)
+
