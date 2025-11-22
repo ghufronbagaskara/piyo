2158139040fda9984d9fc8665121f4da9a6fadf4
ghufronbagaskara <ghufronbagas@student.ub.ac.id>
2025-11-23 00:25:21 +0700

    feat: add EducationContentDto for education data representation

---
+++ app/src/main/java/com/example/piyo/data/dto/education/EducationContentDto.kt
@@ -0,0 +1,15 @@
+package com.example.piyo.data.dto.education
+
+import com.google.firebase.Timestamp
+
+data class EducationContentDto(
+    val id: String? = null,
+    val type: String? = null,
+    val title: String? = null,
+    val thumbnail: String? = null,
+    val description: String? = null,
+    val duration: String? = null,
+    val createdAt: Timestamp? = null,
+    val tags: List<String>? = null
+)
+
