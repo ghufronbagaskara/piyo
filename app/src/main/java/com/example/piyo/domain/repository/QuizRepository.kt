560d75a12c2ad5d786acf2f32089b2b955ad1b0e
ghufronbagaskara <ghufronbagas@student.ub.ac.id>
2025-11-27 17:51:34 +0700

    refactor: update QuizRepository interface

--- app/src/main/java/com/example/piyo/domain/repository/QuizRepository.kt
+++ app/src/main/java/com/example/piyo/domain/repository/QuizRepository.kt
@@ -1,10 +1,12 @@
 package com.example.piyo.domain.repository
 
 import com.example.piyo.domain.model.Quiz
+import com.example.piyo.domain.model.QuizItem
 import kotlinx.coroutines.flow.Flow
 
 interface QuizRepository {
-    fun observeQuizzes(): Flow<Result<List<Quiz>>>
-    suspend fun searchQuizzes(query: String): Result<List<Quiz>>
+    suspend fun getQuizByAgeSegment(ageSegment: String): Result<Quiz>
+    suspend fun getAllQuizzes(): Flow<List<Quiz>>
+    suspend fun observeQuizzes(): Flow<Result<List<QuizItem>>>
+    suspend fun searchQuizzes(query: String): Result<List<QuizItem>>
 }
-
