561e4507a8a637ef816467c809a3a289fb29f9c7
ghufronbagaskara <ghufronbagas@student.ub.ac.id>
2025-11-27 17:51:46 +0700

    feat: update ObserveQuizzesUseCase to use QuizItem model

--- app/src/main/java/com/example/piyo/domain/usecase/quiz/ObserveQuizzesUseCase.kt
+++ app/src/main/java/com/example/piyo/domain/usecase/quiz/ObserveQuizzesUseCase.kt
@@ -1,14 +1,13 @@
 package com.example.piyo.domain.usecase.quiz
 
-import com.example.piyo.domain.model.Quiz
+import com.example.piyo.domain.model.QuizItem
 import com.example.piyo.domain.repository.QuizRepository
 import kotlinx.coroutines.flow.Flow
 
 class ObserveQuizzesUseCase(
     private val repository: QuizRepository
 ) {
-    operator fun invoke(): Flow<Result<List<Quiz>>> {
+    suspend operator fun invoke(): Flow<Result<List<QuizItem>>> {
         return repository.observeQuizzes()
     }
 }
-
