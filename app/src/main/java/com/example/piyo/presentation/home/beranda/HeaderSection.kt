a435da06e227c2b29f15c29b6c2da36cfcb351e4
ghufronbagaskara <ghufronbagas@student.ub.ac.id>
2026-01-06 14:08:09 +0700

    feat: update HeaderSection to display user data and handle search

--- app/src/main/java/com/example/piyo/presentation/home/beranda/HeaderSection.kt
+++ app/src/main/java/com/example/piyo/presentation/home/beranda/HeaderSection.kt
@@ -4,10 +4,18 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
-fun HeaderSection(navController: NavController) {
+fun HeaderSection(
+    navController: NavController,
+    userName: String = "Pengguna",
+    userPhotoUrl: String = "",
+    onSearch: (String) -> Unit = {}
+) {
     ProfileHeader(
+        name = userName,
+        photoUrl = userPhotoUrl,
         onNotificationClick = {
             navController.navigate(com.example.piyo.presentation.navigation.NotifikasiRoute)
-        }
+        },
+        onSearch = onSearch
     )
 }
