ef11963801f6af3e1df8ea7c2c92c2d06102beab
ghufronbagaskara <ghufronbagas@student.ub.ac.id>
2026-01-06 14:10:29 +0700

    feat: add quiz routes to navigation graph

--- app/src/main/java/com/example/piyo/presentation/navigation/TutorialNavigation.kt
+++ app/src/main/java/com/example/piyo/presentation/navigation/TutorialNavigation.kt
@@ -6,12 +6,16 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.example.piyo.presentation.splash.SplashScreen
 import com.example.piyo.presentation.tutorial.TutorialScreen
 import com.example.piyo.presentation.auth.login.LoginScreen
 import com.example.piyo.presentation.auth.register.RegisterScreen
 import com.example.piyo.presentation.home.parent.ChatBotScreen
 import com.example.piyo.presentation.infoanak.InfoAnakScreen
+import com.example.piyo.presentation.quiz.QuizIntroductionScreen
+import com.example.piyo.presentation.quiz.QuizQuestionScreen
+import com.example.piyo.presentation.quiz.QuizResultScreen
 
 @Composable
 fun TutorialNavigation(
@@ -30,6 +34,30 @@ fun TutorialNavigation(
         composable<RegisterRoute> { RegisterScreen(navController) }
         composable<InfoAnakRoute> { InfoAnakScreen(navController) }
         composable<ChatbotRoute> { ChatBotScreen(navController) }
+
+        // Quiz routes
+        composable<QuizIntroductionRoute> { backStackEntry ->
+            val route = backStackEntry.toRoute<QuizIntroductionRoute>()
+            QuizIntroductionScreen(
+                navController = navController,
+                childAge = route.childAge,
+                childId = route.childId
+            )
+        }
+
+        composable<QuizQuestionRoute> { backStackEntry ->
+            val route = backStackEntry.toRoute<QuizQuestionRoute>()
+            QuizQuestionScreen(
+                navController = navController,
+                childAge = route.childAge,
+                childId = route.childId
+            )
+        }
+
+        composable<QuizResultRoute> {
+            QuizResultScreen(navController = navController)
+        }
+
         composable<MainRoute> {
             MainNavigation(
                 navigateLogin = {
